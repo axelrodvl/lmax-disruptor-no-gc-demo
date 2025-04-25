cf93aa6fead7b92cab00960b7537378c3e43c402
Vadim Axelrod <vaxelrod@devexperts.com>
2025-04-25 13:12:42 +0400

    NativeMemoryTracking

--- profiling.sh
+++ profiling.sh
@@ -3,5 +3,6 @@
 java \
   -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC -XX:+AlwaysPreTouch \
   -Xmx64m \
-  -XX:+FlightRecorder -XX:StartFlightRecording=duration=240s,filename=websocket-client-with-disruptor-no-gc.jfr \
+  -XX:+FlightRecorder -XX:StartFlightRecording=duration=60s,filename=websocket-client-with-disruptor-no-gc.jfr \
+  -XX:NativeMemoryTracking=detail \
   -jar target/websocket-client-with-disruptor-no-gc.jar
