d1e9f2bd4276e892cb7031b1b1794231e9e41044
Vadim Axelrod <vaxelrod@devexperts.com>
2025-05-01 01:42:32 +0400

    Benchmark sample

---
+++ benchmark.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+./mvnw clean package
+java \
+  -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC -XX:+AlwaysPreTouch \
+  -Xmx256m \
+  --add-exports=java.base/jdk.internal.ref=ALL-UNNAMED \
+  --add-exports=java.base/sun.nio.ch=ALL-UNNAMED \
+  --add-exports=jdk.unsupported/sun.misc=ALL-UNNAMED \
+  --add-exports=jdk.compiler/com.sun.tools.javac.file=ALL-UNNAMED \
+  --add-opens=jdk.compiler/com.sun.tools.javac=ALL-UNNAMED \
+  --add-opens=java.base/java.lang=ALL-UNNAMED \
+  --add-opens=java.base/java.lang.reflect=ALL-UNNAMED \
+  --add-opens=java.base/java.io=ALL-UNNAMED \
+  --add-opens=java.base/java.util=ALL-UNNAMED \
+  -cp target/websocket-client-with-disruptor-no-gc.jar \
+  org.openjdk.jmh.Main \
+  -prof gc
---
+++ src/main/java/co/axelrod/websocket/client/parser/BinanceBookDepthParserBenchmark.java
@@ -0,0 +1,33 @@
+package co.axelrod.websocket.client.parser;
+
+import net.openhft.chronicle.bytes.Bytes;
+import org.openjdk.jmh.annotations.*;
+
+import java.nio.ByteBuffer;
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Thread)
+public class BinanceBookDepthParserBenchmark {
+    private Bytes<?> bytes;
+
+    @Setup(Level.Iteration)
+    public void setup() {
+        ByteBuffer buffer = ByteBuffer.allocateDirect(32);
+        buffer.put("12345.6789".getBytes());
+        buffer.flip();
+        bytes = Bytes.wrapForRead(buffer);
+    }
+
+    @Benchmark
+    public double parseDoubleNoGc() {
+        bytes.readPosition(0); // reset to start
+        return bytes.parseDouble();
+    }
+
+    @TearDown(Level.Iteration)
+    public void tearDown() {
+        bytes.releaseLast();
+    }
+}
